a2b8fba9d52931db86a006122b57ffa2f131bdc8
Tugario <>
2020-09-14 16:34:02 -0700

    First commit

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Nicole Nguyen on 9/10/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Nicole Nguyen on 9/10/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
